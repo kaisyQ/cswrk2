b982cf119f80bc7730dfe9fb376f5b4314c59c69
kaisyQ <85626015+kaisyQ@users.noreply.github.com>
2021-12-01 11:56:11 +1000

    Add files via upload

--- Auth/auth.js
+++ Auth/auth.js
@@ -1,27 +1,30 @@
+const electron = require('electron');
 const postgres = require('pg');
 const client = new postgres.Client(require('./config'));
 
-const CheckUserInDatabase = (user) => {
+const CheckUserInDatabase = async (user) => {
 
     try {
-        client.connect();
+        await client.connect();
     } catch (err) {
         console.error(err);
     }
 
-    client.query('select * from users;', (err, res) => {
-        if(err) {
-            console.error(err);
-        }
-        client.end();
+    const con = await client.query('select * from users');
 
-        for (dt in res.rows) {
-            if (dt.name === user.username && dt.password === user.password) {
+    if (con) {
+        for (let i = 0; i < con.rows.length; ++i) {
+            if (con.rows[i].email === user.username && con.rows[i].psswrd == user.password) {
+                await client.end();
                 return true;
             }
         }
+        await client.end();
         return false;
-    })
+    } else {
+        console.error('Query error !');
+    }
 }; 
+
 
 module.exports = CheckUserInDatabase;
